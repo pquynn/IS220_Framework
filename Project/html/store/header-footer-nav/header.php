b104828e6fdd9f1cbb55af81b5e7db536804a5fa
Phuong Quyen <pquyen245@gmail.com>
2023-11-11 00:22:26 +0700

    add bootstrap to header of store

--- Project/html/store/header-footer-nav/header.php
+++ Project/html/store/header-footer-nav/header.php
@@ -7,6 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php echo $title; ?></title>
 
+     <!-- bootstrap -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  
     <!-- css file -->
     <link rel="stylesheet" href="../../../css/style-components/base.css" />
     <link rel="stylesheet" href="../../../css/style-components/header.css" />
@@ -25,8 +28,7 @@
       @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap");
     </style>
 
-
-  </head>
+   </head>
   <body>
     <ul class="header">
       <!-- LOGO: start -->
@@ -37,7 +39,7 @@
 
       <!-- LINK (HOME, SHOP, CATEGORY,....): Start -->
       <ul class="nav-link">
-        <li class="nav-link--item hover-underline"><a href="../homepage-shopping/homepage.html">TRANG CHỦ</a></li>
+        <li class="nav-link--item hover-underline"><a href="#">TRANG CHỦ</a></li>
         <li class="nav-link--item hover-underline"><a href="#">MUA SẮM</a></li>
 
         <!-- category sub navigation -->
